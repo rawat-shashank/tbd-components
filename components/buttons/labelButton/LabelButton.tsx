4e7e7333c85a0b4928d2022a1df8acc23b3745b7
rawat-shashank <shashank17nov@gmail.com>
2021-12-17 19:54:58 +0530

    feat(components): add label-button component
    adding a new component named label-button, can be used with or without
    icon
    
    Closes #20

---
+++ components/buttons/labelButton/LabelButton.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Icon } from '../../icons/Icon';
+import { StyledLabelButton } from './LabelButton.styles';
+import { LabelButtonProps } from './LabelButton.types';
+
+export const LabelButton = ({
+  label = 'Label Button',
+  icon,
+  className,
+  style,
+  variant = 'primary',
+  disabled,
+}: LabelButtonProps) => {
+  const classes = className ? [className] : [];
+  switch (variant) {
+    case 'primary':
+      if (disabled) break;
+      classes.push('primary');
+      break;
+
+    case 'outline':
+      classes.push('outline');
+      break;
+
+    case 'active':
+      classes.push('active');
+      break;
+
+    default:
+      break;
+  }
+
+  return (
+    <StyledLabelButton
+      className={classes.length && classes.join(' ')}
+      style={style}
+      disabled={disabled}
+      data-testid="label-button"
+    >
+      {icon && (
+        <Icon
+          name={icon}
+          height="1.5rem"
+          width="1.5rem"
+          style={{ marginRight: '0.75rem' }}
+        />
+      )}
+      {label}
+    </StyledLabelButton>
+  );
+};
