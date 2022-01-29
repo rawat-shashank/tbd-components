4bf15e3359b9aec6fc61702c70eb5a9a4bc8bacc
rawat-shashank <shashank17nov@gmail.com>
2022-01-29 19:24:20 +0530

    chore(storybook): add themes in storybook previews
    this will add global context for storybook, which will help preview all components with themes

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,46 +1,36 @@
-// export const parameters = {
-//   actions: { argTypesRegex: "^on[A-Z].*" },
-//   controls: {
-//     matchers: {
-//       color: /(background|color)$/i,
-//       date: /Date$/,
-//     },
-//   },
-// }
-
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from '../styles/globalStyles';
 import { lightTheme, darkTheme } from '../styles/themes';
 
-export const decorators = [
-  (Story) => (
-    <ThemeProvider theme={lightTheme}>
-      <GlobalStyles />
-      <Story />
-    </ThemeProvider>
-  ),
-];
-
-export const parameters = {
-  actions: { argTypesRegex: '^on[A-Z].*' },
-  backgrounds: {
-    default: 'lightTheme',
-    values: [
-      {
-        name: 'lightTheme',
-        value: '#F3F3F3',
-      },
-      {
-        name: 'darkTheme',
-        value: '#000000',
-      },
-    ],
-  },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for components',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'circlehollow',
+      items: ['light', 'dark'],
+      showName: true,
     },
   },
 };
+
+const getTheme = (themeName) => {
+  const themes = {
+    'light': lightTheme,
+    'dark': darkTheme
+  }
+  return themes[themeName]
+}
+
+const withThemeProvider = (Story, context) => {
+  const theme = getTheme(context.globals.theme);
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Story {...context} />
+    </ThemeProvider>
+  )
+}
+export const decorators = [withThemeProvider];
